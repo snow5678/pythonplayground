971e5382a21308058f2252dc911d38cd7e2113a0
snow5678 <sheauying.yong@gmail.com>
2021-12-19 22:14:05 +0800

    sample commit message

--- test.py
+++ test.py
@@ -1,2 +1,14 @@
 def question1():
-    print("remove this line and put your answer here")
+	number = float(input("Enter a number"))
+	if number == 3:
+		print ('third')
+	elif number == 2:
+		print ('second')
+	elif number == 1:
+		print ('first')
+	elif number == 4 and number == 5:
+		print ('top 5')		
+	elif number >= 6 and number <= 10:
+		print ('top 10')
+	else 
+		print ('Invalid Number')
